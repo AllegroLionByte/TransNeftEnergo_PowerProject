36eae6b5c702a9949972db6bfe6c2cb46d70c4da
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-20 18:05:28 +0300

    Completed project top-level structure.

---
+++ TNEPowerProject.APITestConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TNEPowerProject.APITestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
