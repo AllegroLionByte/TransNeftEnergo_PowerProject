1354637fc327d052f547505e66acfe5117b536b0
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-21 16:27:26 +0300

    Добавил интерфейс и реализацию для репозитория трансформаторов напряжения.

---
+++ TNEPowerProject.Domain/Repository/IVoltageTransformersRepository.cs
@@ -0,0 +1,11 @@
+﻿using TNEPowerProject.Domain.Entities;
+
+namespace TNEPowerProject.Domain.Repository
+{
+    /// <summary>
+    /// Представляет интерфейс для репозитория трансформаторов напряжения
+    /// </summary>
+    public interface IVoltageTransformersRepository : ITNERepository<VoltageTransformer>
+    {
+    }
+}
---
+++ TNEPowerProject.Infrastructure/Repository/VoltageTransformersRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TNEPowerProject.Domain.Entities;
+using TNEPowerProject.Domain.Repository;
+
+namespace TNEPowerProject.Infrastructure.Repository
+{
+    /// <summary>
+    /// Представляет реализацию репозитория трансформаторов напряжения
+    /// </summary>
+    public class VoltageTransformersRepository : TNERepository<VoltageTransformer>, IVoltageTransformersRepository
+    {
+        /// <summary>
+        /// Представляет реализацию репозитория трансформаторов напряжения
+        /// </summary>
+        public VoltageTransformersRepository(DbContext dbContext, ILogger logger) : base(dbContext, logger)
+        {
+
+        }
+
+    }
+}
