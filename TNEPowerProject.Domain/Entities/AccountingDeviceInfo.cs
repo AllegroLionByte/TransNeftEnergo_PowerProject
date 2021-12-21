f5c0d71a6f6816d6f3a671da3b201d0fc8897181
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-21 12:41:40 +0300

    Добавлена сущность для расчётного прибора учёта.

---
+++ TNEPowerProject.Domain/Entities/AccountingDeviceInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TNEPowerProject.Domain.Entities
+{
+    /// <summary>
+    /// Представляет описание расчётного прибора учёта
+    /// </summary>
+    public class AccountingDeviceInfo
+    {
+        /// <summary>
+        /// Уникальный идентификатор расчётного прибора учёта
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Указывает начало расчётного периода
+        /// </summary>
+        public DateTime Interval_From { get; set; }
+        /// <summary>
+        /// Указывает конец расчётного периода
+        /// </summary>
+        public DateTime Interval_To { get; set; }
+        /// <summary>
+        /// Указывает количество потреблённой электроэнергии за расчётный период
+        /// </summary>
+        public double ConsumedElectricity { get; set; }
+        /// <summary>
+        /// Указывает на Id использовавшейся точки поставки электроэнергии
+        /// </summary>
+        public int ElectricitySupplyPointId { get; set; }
+        /// <summary>
+        /// Указывает на использовавшуюся точку поставки электроэнергии
+        /// </summary>
+        public ElectricitySupplyPoint SupplyPoint { get; set; }
+        /// <summary>
+        /// Указывает на Id использовавшейся точки измерения электроэнергии
+        /// </summary>
+        public int ElectricityMeasuringPointId { get; set; }
+        /// <summary>
+        /// Указывает на использовавшуюся точку измерения электроэнергии
+        /// </summary>
+        public ElectricityMeasuringPoint MeasuringPoint { get; set; }
+    }
+}
