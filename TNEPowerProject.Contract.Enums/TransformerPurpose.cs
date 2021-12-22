411381aa5ce6e77a1fd4a83d43b3d6f094afb4b1
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-22 18:22:03 +0300

    ADD Contract.Enums Добавлены перечисления для Restful Response Codes и типа трансформатора для DTO. Перечисления выделены в новый созданный проект.

---
+++ TNEPowerProject.Contract.Enums/TransformerPurpose.cs
@@ -0,0 +1,17 @@
+﻿namespace TNEPowerProject.Contract.Enums
+{
+    /// <summary>
+    /// Представляет тип трансформатора
+    /// </summary>
+    public enum TransformerPurpose
+    {
+        /// <summary>
+        /// Трансформатор тока
+        /// </summary>
+        CurrentTransformer = 0,
+        /// <summary>
+        /// Трансформатор напряжения
+        /// </summary>
+        VoltageTransformer = 1
+    }
+}
