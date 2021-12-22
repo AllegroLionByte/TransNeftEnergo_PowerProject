c8abe616be1d3792d27d3320d3a1650f02e2f9fb
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-22 19:02:58 +0300

    ADD Добавил API для типов трансформаторов.

---
+++ TNEPowerProject.Contract.Interfaces/ITransformerTypesAPI.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TNEPowerProject.Contract.DTO.Transformers;
+
+namespace TNEPowerProject.Contract.Interfaces
+{
+    /// <summary>
+    /// Представляет описание API для типов трансформаторов
+    /// </summary>
+    public interface ITransformerTypesAPI
+    {
+        /// <summary>
+        /// Метод для создания нового типа трансформатора
+        /// </summary>
+        /// <param name="transformerTypeDTO"></param>
+        /// <returns></returns>
+        [HttpPost]
+        Task<TransformerTypeDTO> CreateTransformerType(TransformerTypeDTO transformerTypeDTO);
+    }
+}
