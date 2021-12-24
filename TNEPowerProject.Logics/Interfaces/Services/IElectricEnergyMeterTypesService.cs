49233fa7b15c0dac78a8f07414a2d9a7e86844fb
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-24 18:32:04 +0300

    ADD Добавлены сервисы для счётчиков электрической энергии.

---
+++ TNEPowerProject.Logics/Interfaces/Services/IElectricEnergyMeterTypesService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using TNEPowerProject.Contract.DTO.ElectricEnergyMeterTypes;
+
+namespace TNEPowerProject.Logics.Interfaces.Services
+{
+    /// <summary>
+    /// Представляет интерфейс сервиса для типов счётчиков электрической энергии
+    /// </summary>
+    public interface IElectricEnergyMeterTypesService
+    {
+        /// <summary>
+        /// Позволяет добавить новый тип счётчика электрической энергии
+        /// </summary>
+        Task<ElectricEnergyMeterTypeDTO> CreateElectricEnergyMeterType(CreateElectricEnergyMeterTypeDTO createElectricEnergyMeterTypeDTO);
+        /// <summary>
+        /// Позволяет получить список всех типов счётчиков электрической энергии
+        /// </summary>
+        Task<ElectricEnergyMeterTypesListDTO> GetAllElectricEnergyMeterTypes();
+        /// <summary>
+        /// Позволяет проверить существование типа счётчика электрической энергии с указанным Id
+        /// </summary>
+        /// <param name="electricEnergyMeterTypeId">
+        /// Id типа счётчика электрической энергии
+        /// </param>
+        Task<ElectricEnergyMeterTypeExistenceDTO> CheckElectricEnergyMeterTypeExists(int electricEnergyMeterTypeId);
+    }
+}
---
+++ TNEPowerProject.Logics/Services/ElectricEnergyMeterTypesService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using TNEPowerProject.Domain.Entities;
+using TNEPowerProject.Contract.Enums;
+using TNEPowerProject.Infrastructure.Repository;
+using TNEPowerProject.Logics.Interfaces.Services;
+using TNEPowerProject.Infrastructure.Database.EFCore;
+using TNEPowerProject.Contract.DTO.ElectricEnergyMeterTypes;
+
+namespace TNEPowerProject.Logics.Services
+{
+    /// <summary>
+    /// Представляет реализацию сервиса для типов счётчиков электрической энергии
+    /// </summary>
+    public class ElectricEnergyMeterTypesService : IElectricEnergyMeterTypesService
+    {
+        private readonly ElectricEnergyMeterTypesRepository electricEnergyMeterTypesRepository;
+        /// <summary>
+        /// Представляет реализацию сервиса для типов счётчиков электрической энергии
+        /// </summary>
+        public ElectricEnergyMeterTypesService(EnergoDBContext dbContext) : this(dbContext, null) { }
+        /// <summary>
+        /// Представляет реализацию сервиса для типов счётчиков электрической энергии
+        /// </summary>
+        public ElectricEnergyMeterTypesService(EnergoDBContext dbContext, ILogger logger)
+        {
+            electricEnergyMeterTypesRepository = new ElectricEnergyMeterTypesRepository(dbContext, logger);
+        }
+        /// <summary>
+        /// Позволяет добавить новый тип счётчика электрической энергии
+        /// </summary>
+        public async Task<ElectricEnergyMeterTypeDTO> CreateElectricEnergyMeterType(CreateElectricEnergyMeterTypeDTO createElectricEnergyMeterTypeDTO)
+        {
+            if (string.IsNullOrWhiteSpace(createElectricEnergyMeterTypeDTO.Description))
+            {
+                return new ElectricEnergyMeterTypeDTO(RestResponseCode.BadRequest, "Не указано описание (название) типа счётчика электрической энергии.");
+            }
+            ElectricEnergyMeterType createEEMTResult = await electricEnergyMeterTypesRepository.Add(new ElectricEnergyMeterType()
+            {
+                Description = createElectricEnergyMeterTypeDTO.Description.Trim()
+            });
+            if (createEEMTResult == null)
+            {
+                return new ElectricEnergyMeterTypeDTO(RestResponseCode.InternalServerError, "Произошла ошибка при попытке добавления нового типа счётчика электрической энергии.");
+            }
+            else
+            {
+                return new ElectricEnergyMeterTypeDTO(RestResponseCode.Created)
+                {
+                    Id = createEEMTResult.Id,
+                    Description = createEEMTResult.Description
+                };
+            }
+        }
+        /// <summary>
+        /// Позволяет проверить существование типа счётчика электрической энергии с указанным Id
+        /// </summary>
+        /// <param name="electricEnergyMeterTypeId">
+        /// Id типа счётчика электрической энергии
+        /// </param>
+        public async Task<ElectricEnergyMeterTypeExistenceDTO> CheckElectricEnergyMeterTypeExists(int electricEnergyMeterTypeId)
+        {
+            return new ElectricEnergyMeterTypeExistenceDTO(RestResponseCode.OK)
+            {
+                Exists = await electricEnergyMeterTypesRepository.Exists(x => x.Id == electricEnergyMeterTypeId)
+            };
+        }
+        /// <summary>
+        /// Позволяет получить список всех типов счётчиков электрической энергии
+        /// </summary>
+        public async Task<ElectricEnergyMeterTypesListDTO> GetAllElectricEnergyMeterTypes()
+        {
+            return new ElectricEnergyMeterTypesListDTO(RestResponseCode.OK)
+            {
+                TransformerTypes = (await electricEnergyMeterTypesRepository.GetAll()).Select(x => new ElectricEnergyMeterTypesListDTO.ElectricEnergyMeterTypeListItemDTO()
+                {
+                    Id = x.Id,
+                    Description = x.Description
+                }).ToList()
+            };
+        }
+    }
+}
