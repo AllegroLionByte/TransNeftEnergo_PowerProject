e4a2451e17aec36e0fb0856993a481d0e983931c
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-24 17:54:28 +0300

    FIX Исправлена очерёдность полей в классе, добавлена возможность создания DTO с указанием сообщения о статусе операции.

--- TNEPowerProject.Contract.DTO/Transformers/TransformerTypesListDTO.cs
+++ TNEPowerProject.Contract.DTO/Transformers/TransformerTypesListDTO.cs
@@ -14,7 +14,17 @@ namespace TNEPowerProject.Contract.DTO.Transformers
         /// <param name="responseCode">
         /// Статус операции из перечисления TNEPowerProject.Contract.Enums.RestResponseCode
         /// </param>
-        public TransformerTypesListDTO(RestResponseCode responseCode) : base(responseCode)
+        public TransformerTypesListDTO(RestResponseCode responseCode) : this(responseCode, "") { }
+        /// <summary>
+        /// Представляет DTO для списка типов трансформаторов
+        /// </summary>
+        /// <param name="responseCode">
+        /// Статус операции из перечисления TNEPowerProject.Contract.Enums.RestResponseCode
+        /// </param>
+        /// <param name="message">
+        /// Сообщение о статусе операции
+        /// </param>
+        public TransformerTypesListDTO(RestResponseCode responseCode, string message) : base(responseCode, message)
         {
             TransformerTypes = new List<TransformerTypeListItemDTO>();
         }
@@ -28,13 +38,13 @@ namespace TNEPowerProject.Contract.DTO.Transformers
         public class TransformerTypeListItemDTO
         {
             /// <summary>
-            /// Род работы трансформатора
-            /// </summary>
-            public int TransformerPurpose { get; set; }
-            /// <summary>
             /// Уникальный идентификатор типа трансформатора
             /// </summary>
             public int Id { get; set; }
+            /// <summary>
+            /// Род работы трансформатора
+            /// </summary>
+            public int TransformerPurpose { get; set; }
             /// <summary>
             /// Описание (название) типа трансформатора
             /// </summary>
