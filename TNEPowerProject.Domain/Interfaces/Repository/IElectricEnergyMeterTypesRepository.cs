ab713308e83a92ae3b465eb05b045c697f545f5c
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-24 18:31:16 +0300

    ADD Добавлены репозитории для счётчиков электрической энергии.

---
+++ TNEPowerProject.Domain/Interfaces/Repository/IElectricEnergyMeterTypesRepository.cs
@@ -0,0 +1,11 @@
+﻿using TNEPowerProject.Domain.Entities;
+
+namespace TNEPowerProject.Domain.Interfaces.Repository
+{
+    /// <summary>
+    /// Представляет интерфейс для репозитория типов трансформаторов
+    /// </summary>
+    public interface IElectricEnergyMeterTypesRepository : ITNERepository<ElectricEnergyMeterType>
+    {
+    }
+}
---
+++ TNEPowerProject.Infrastructure/Repository/ElectricEnergyMeterTypesRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TNEPowerProject.Domain.Entities;
+using TNEPowerProject.Domain.Interfaces.Repository;
+
+namespace TNEPowerProject.Infrastructure.Repository
+{
+    /// <summary>
+    /// Представляет реализацию репозитория типов счётчиков электрической энергии
+    /// </summary>
+    public class ElectricEnergyMeterTypesRepository : TNERepository<ElectricEnergyMeterType>, IElectricEnergyMeterTypesRepository
+    {
+        /// <summary>
+        /// Представляет реализацию репозитория типов счётчиков электрической энергии
+        /// </summary>
+        public ElectricEnergyMeterTypesRepository(DbContext dbContext, ILogger logger) : base(dbContext, logger) { }
+    }
+}
