01f7f3b311e1bd84eee8578325e70ac50adabfba
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-24 10:52:35 +0300

    ADD Добавлен паттерн UnitOfWork для генерирования репозиториев "на лету".

---
+++ TNEPowerProject.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using TNEPowerProject.Domain.Abstract;
+using TNEPowerProject.Domain.Interfaces.Repository;
+
+namespace TNEPowerProject.Domain.Interfaces
+{
+    /// <summary>
+    /// Представляет интерфейс для паттерна Unit of Work
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Позволяет получить объект репозитория
+        /// </summary>
+        ITNERepository<T> AsyncRepository<T>() where T : TNEEntityBase;
+        /// <summary>
+        /// Позволяет применить изменения к БД
+        /// </summary>
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ TNEPowerProject.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using TNEPowerProject.Domain.Abstract;
+using TNEPowerProject.Domain.Interfaces;
+using TNEPowerProject.Domain.Interfaces.Repository;
+using TNEPowerProject.Infrastructure.Database.EFCore;
+
+namespace TNEPowerProject.Infrastructure.Repository
+{
+    /// <summary>
+    /// Представляет реализацию для паттерна Unit of Work
+    /// </summary>
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly EnergoDBContext dbContext;
+        private readonly ILogger logger;
+        /// <summary>
+        /// Представляет реализацию для паттерна Unit of Work
+        /// </summary>
+        public UnitOfWork(EnergoDBContext dbContext, ILogger logger)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+        }
+        /// <summary>
+        /// Позволяет получить объект репозитория
+        /// </summary>
+        public ITNERepository<T> AsyncRepository<T>() where T : TNEEntityBase
+        {
+            return new TNERepository<T>(dbContext, logger);
+        }
+        /// <summary>
+        /// Позволяет применить изменения к БД
+        /// </summary>
+        public Task<int> SaveChangesAsync()
+        {
+            return dbContext.SaveChangesAsync();
+        }
+    }
+}
