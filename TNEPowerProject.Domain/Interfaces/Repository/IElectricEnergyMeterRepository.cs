4ea2bfa649e96293d32a39d155719e78216d2fcd
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-27 17:20:01 +0300

    ADD Добавлены интерфейс и реализация репозитория для счётчиков электрической энергии.

---
+++ TNEPowerProject.Domain/Interfaces/Repository/IElectricEnergyMeterRepository.cs
@@ -0,0 +1,11 @@
+﻿using TNEPowerProject.Domain.Entities;
+
+namespace TNEPowerProject.Domain.Interfaces.Repository
+{
+    /// <summary>
+    /// Представляет интерфейс для репозитория счётчиков электрической энергии
+    /// </summary>
+    public interface IElectricEnergyMeterRepository : ITNERepository<ElectricEnergyMeter>
+    {
+    }
+}
---
+++ TNEPowerProject.Infrastructure/Repository/ElectricEnergyMetersRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TNEPowerProject.Domain.Entities;
+using TNEPowerProject.Domain.Interfaces.Repository;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace TNEPowerProject.Infrastructure.Repository
+{
+    /// <summary>
+    /// Представляет реализацию репозитория счётчиков электрической энергии
+    /// </summary>
+    public class ElectricEnergyMetersRepository : TNERepository<ElectricEnergyMeter>, IElectricEnergyMeterRepository
+    {
+        /// <summary>
+        /// Представляет реализацию репозитория счётчиков электрической энергии
+        /// </summary>
+        public ElectricEnergyMetersRepository(DbContext dbContext, ILogger logger) : base(dbContext, logger) { }
+        /// <summary>
+        /// Позволяет найти счётчики электрической энергии по заданному условию (в ответ включается Тип счётчика электрической энергии)
+        /// </summary>
+        public override async Task<IEnumerable<ElectricEnergyMeter>> Find(Expression<Func<ElectricEnergyMeter, bool>> predicate)
+        {
+            try
+            {
+                return await dbContext.Set<ElectricEnergyMeter>().Where(predicate).Include(x => x.EEMeterType).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex, "{Repo}: error during finding elements {Entity} by {Pred}.", GetType(), typeof(ElectricEnergyMeter), predicate);
+                return new List<ElectricEnergyMeter>();
+            }
+        }
+        /// <summary>
+        /// Позволяет получить список всех объектов
+        /// </summary>
+        public async override Task<IEnumerable<ElectricEnergyMeter>> GetAll()
+        {
+            try
+            {
+                return await dbContext.Set<ElectricEnergyMeter>().Include(x => x.EEMeterType).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex, "{Repo}: error during getting all elements {Entity}.", GetType(), typeof(ElectricEnergyMeter));
+                return new List<ElectricEnergyMeter>();
+            }
+        }
+        /// <summary>
+        /// Позволяет получить счётчик электрической энергии по его Id
+        /// </summary>
+        public async override Task<ElectricEnergyMeter> GetById(int id)
+        {
+            try
+            {
+                return await dbContext.Set<ElectricEnergyMeter>().Include(x => x.EEMeterType).FirstOrDefaultAsync(x => x.Id == id);
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex, "{Repo}: error during getting element {Entity} by id: {Id}.", GetType(), typeof(ElectricEnergyMeter), id);
+                return null;
+            }
+        }
+    }
+}
