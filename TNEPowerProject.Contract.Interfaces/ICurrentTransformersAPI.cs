8a525a1d9c8f977a427ebf738dbe23b90a1452b7
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-27 19:04:50 +0300

    ADD Добавлены интерфейсы API для трансформаторов тока и напряжения.

---
+++ TNEPowerProject.Contract.Interfaces/ICurrentTransformersAPI.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TNEPowerProject.Contract.DTO.Transformers;
+
+namespace TNEPowerProject.Contract.Interfaces
+{
+    /// <summary>
+    /// Представляет описание API для трансформаторов тока
+    /// </summary>
+    public interface ICurrentTransformersAPI
+    {
+        /// <summary>
+        /// Метод для создания нового трансформатора тока
+        /// </summary>
+        /// <param name="createCurrentTransformerDTO">
+        /// DTO для нового трансформатора тока
+        /// </param>
+        [HttpPost]
+        Task<CurrentTransformerDTO> CreateCurrentTransformer(CreateCurrentTransformerDTO createCurrentTransformerDTO);
+        /// <summary>
+        /// Метод для проверки существования трансформатора тока с указанным Id
+        /// </summary>
+        /// <param name="currentTransformerId">
+        /// Id трансформатора тока
+        /// </param>
+        [HttpGet("exists")]
+        Task<CurrentTransformerExistenceDTO> CheckCurrentTransformerExists(int currentTransformerId);
+    }
+}
---
+++ TNEPowerProject.Contract.Interfaces/IVoltageTransformersAPI.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TNEPowerProject.Contract.DTO.Transformers;
+
+namespace TNEPowerProject.Contract.Interfaces
+{
+    /// <summary>
+    /// Представляет описание API для трансформаторов напряжения
+    /// </summary>
+    public interface IVoltageTransformersAPI
+    {
+        /// <summary>
+        /// Метод для создания нового трансформатора напряжения
+        /// </summary>
+        /// <param name="createVoltageTransformerDTO">
+        /// DTO для нового трансформатора напряжения
+        /// </param>
+        [HttpPost]
+        Task<VoltageTransformerDTO> CreateVoltageTransformer(CreateVoltageTransformerDTO createVoltageTransformerDTO);
+        /// <summary>
+        /// Метод для проверки существования трансформатора напряжения с указанным Id
+        /// </summary>
+        /// <param name="voltageTransformerId">
+        /// Id трансформатора напряжения
+        /// </param>
+        [HttpGet("exists")]
+        Task<VoltageTransformerExistenceDTO> CheckVoltageTransformerExists(int voltageTransformerId);
+    }
+}
