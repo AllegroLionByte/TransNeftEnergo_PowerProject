f8028ffbe530d387985af5e3c942cad6a14973f4
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-27 12:36:16 +0300

    FIX Исправил комментарий.

--- TNEPowerProject.Domain/Interfaces/Repository/IElectricEnergyMeterTypesRepository.cs
+++ TNEPowerProject.Domain/Interfaces/Repository/IElectricEnergyMeterTypesRepository.cs
@@ -3,7 +3,7 @@
 namespace TNEPowerProject.Domain.Interfaces.Repository
 {
     /// <summary>
-    /// Представляет интерфейс для репозитория типов трансформаторов
+    /// Представляет интерфейс для репозитория типов счётчиков электрической энергии
     /// </summary>
     public interface IElectricEnergyMeterTypesRepository : ITNERepository<ElectricEnergyMeterType>
     {
