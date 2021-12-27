37abccc7f2029db2b77eaa38a2b23bd3f51e7168
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-27 17:21:05 +0300

    ADD Добавлены API и контроллер для счётчиков электрической энергии.

---
+++ TNEPowerProject.Contract.Interfaces/IElectricEnergyMetersAPI.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TNEPowerProject.Contract.DTO.ElectricEnergyMeters;
+
+namespace TNEPowerProject.Contract.Interfaces
+{
+    /// <summary>
+    /// Представляет описание API для счётчиков электрической энергии
+    /// </summary>
+    public interface IElectricEnergyMetersAPI
+    {
+        /// <summary>
+        /// Метод для создания нового счётчика электрической энергии
+        /// </summary>
+        /// <param name="createElectricEnergyMeterDTO">
+        /// DTO для нового счётчика электрической энергии
+        /// </param>
+        [HttpPost]
+        Task<ElectricEnergyMeterDTO> CreateElectricEnergyMeter(CreateElectricEnergyMeterDTO createElectricEnergyMeterDTO);
+        /// <summary>
+        /// Метод для получения списка всех счётчиков электрической энергии
+        /// </summary>
+        [HttpGet("list")]
+        Task<ElectricEnergyMetersListDTO> GetAll();
+        /// <summary>
+        /// Метод для проверки существования счётчика электрической энергии с указанным Id
+        /// </summary>
+        /// <param name="electricEnergyMeterId">
+        /// Id счётчика электрической энергии
+        /// </param>
+        [HttpGet("exists")]
+        Task<ElectricEnergyMeterExistenceDTO> CheckElectricEnergyMeterExists(int electricEnergyMeterId);
+    }
+}
---
+++ TNEPowerProject.WebAPI/Controllers/ElectricEnergyMetersController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TNEPowerProject.Contract.Interfaces;
+using TNEPowerProject.Logics.Interfaces.Services;
+using TNEPowerProject.Contract.DTO.ElectricEnergyMeters;
+using TNEPowerProject.Contract.DTO.ElectricEnergyMeterTypes;
+
+namespace TNEPowerProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElectricEnergyMetersController : ControllerBase, IElectricEnergyMetersAPI
+    {
+        private readonly IElectricEnergyMetersService electricEnergyMetersService;
+        public ElectricEnergyMetersController(IElectricEnergyMetersService electricEnergyMetersService)
+        {
+            this.electricEnergyMetersService = electricEnergyMetersService;
+        }
+        [HttpGet("exists")]
+        public async Task<ElectricEnergyMeterExistenceDTO> CheckElectricEnergyMeterExists(int electricEnergyMeterId)
+        {
+            return await electricEnergyMetersService.CheckElectricEnergyMeterExists(electricEnergyMeterId);
+        }
+        [HttpPost]
+        public async Task<ElectricEnergyMeterDTO> CreateElectricEnergyMeter(CreateElectricEnergyMeterDTO createElectricEnergyMeterDTO)
+        {
+            return await electricEnergyMetersService.CreateElectricEnergyMeter(createElectricEnergyMeterDTO);
+        }
+        [HttpGet("list")]
+        public async Task<ElectricEnergyMetersListDTO> GetAll()
+        {
+            return await electricEnergyMetersService.GetAllElectricEnergyMeters();
+        }
+    }
+}
