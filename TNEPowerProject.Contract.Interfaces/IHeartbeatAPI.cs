303a2fbd0fca8d20d493e9dee28cb57f89cf3178
LionByte Allegro <allegrolionbyte@hotmail.com>
2021-12-28 13:13:24 +0300

    ADD Добавлен функционал heartbeat для API.

---
+++ TNEPowerProject.Contract.Interfaces/IHeartbeatAPI.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TNEPowerProject.Contract.Interfaces
+{
+    /// <summary>
+    /// Представляет API для проверки жизнеспособности API
+    /// </summary>
+    public interface IHeartbeatAPI
+    {
+        /// <summary>
+        /// Метод для проверки жизнеспособности API. Возвращает строку вида I am alive! Your string: [переданная_строка]
+        /// </summary>
+        /// <param name="checkingString">Строка, которая будет возвращена в ответ</param>
+        [HttpGet("{checkingString}")]
+        [Get("/api/Heartbeat/{checkingString}")]
+        Task<string> Heartbeat(string checkingString);
+    }
+}
---
+++ TNEPowerProject.WebAPI/Controllers/HeartbeatController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TNEPowerProject.Contract.Interfaces;
+
+namespace TNEPowerProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeartbeatController : ControllerBase, IHeartbeatAPI
+    {
+        [HttpGet("{checkingString}")]
+        public Task<string> Heartbeat(string checkingString)
+        {
+            return Task.FromResult($"I am alive! Your string: [{checkingString}]");
+        }
+    }
+}
